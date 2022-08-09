bb6215156fbfde0f71c35b6e6a834184cc66a797
Petar Stokić <5ar100kic@gmail.com>
2022-08-09 18:15:11 +0200

    add ammojs physics plugin

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,23 @@
 import { Engine } from '@babylonjs/core'
 import { createGrabScene } from './scenes/grab-scene'
 import { createShootingScene } from './scenes/shooting-scene'
-import { physicsPlugin } from './setup-physics-plugin'
+import { getPhysicsPlugin, PhysicsEngine } from './setup-physics-plugin'
 
 const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement // Get the canvas element
-const engine = new Engine(canvas, true) // Generate the BABYLON 3D engine
+const engine = new Engine(canvas, true)
 
-const createScene = true
+const createScene = false
   ? createGrabScene
   : createShootingScene
 
-const scenePromise = createScene({engine, physicsPlugin, canvas}) //Call the createScene function
+const scenePromise = createScene({engine, physicsPlugin: await getPhysicsPlugin(PhysicsEngine.Ammo), canvas})
 
-// Register a render loop to repeatedly render the scene
 engine.runRenderLoop(function () {
   scenePromise.then((scene) => {
     scene.render()
   })
 })
 
-// Watch for browser/canvas resize events
 window.addEventListener('resize', function () {
   engine.resize()
 })
--- src/setup-physics-plugin.ts
+++ src/setup-physics-plugin.ts
@@ -1,14 +1,27 @@
-import { CannonJSPlugin, OimoJSPlugin } from '@babylonjs/core'
+import { AmmoJSPlugin, CannonJSPlugin, OimoJSPlugin } from '@babylonjs/core'
 import { IPhysicsEnginePlugin } from '@babylonjs/core/Physics/IPhysicsEngine'
 import * as CANNON from 'cannon'
 // @ts-ignore
 import * as OIMO from 'oimo'
 
-window.CANNON = CANNON
+export enum PhysicsEngine {
+  CANNON = 'cannon',
+  OIMO = 'oimo',
+  Ammo = 'ammo'
+}
 
-// @ts-ignore
-window.OIMO = OIMO
-
-export const physicsPlugin: IPhysicsEnginePlugin = true
-  ? new CannonJSPlugin(undefined, 5)
-  : new OimoJSPlugin(undefined, 10)
+export const getPhysicsPlugin = async (plugin: PhysicsEngine): Promise<IPhysicsEnginePlugin> => {
+  switch (plugin) {
+    case PhysicsEngine.CANNON:
+      window.CANNON = CANNON
+      return new CannonJSPlugin(undefined, 5)
+    case PhysicsEngine.OIMO:
+      // @ts-ignore
+      window.OIMO = OIMO
+      return new OimoJSPlugin(undefined, 10)
+    case PhysicsEngine.Ammo:
+      // @ts-ignore
+      await Ammo()
+      return new AmmoJSPlugin()
+  }
+}
