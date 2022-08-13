e73c51b2ecd061918222529b8434f9d7d9310f9a
Petar Stokić <5ar100kic@gmail.com>
2022-08-13 15:04:23 +0200

    fixup! add biderectional sensor hit support

--- src/hoops/hoop.ts
+++ src/hoops/hoop.ts
@@ -1,4 +1,4 @@
-import { AbstractMesh, Material, MeshBuilder, Scene, TransformNode, Vector3 } from '@babylonjs/core'
+import { AbstractMesh, Material, MeshBuilder, Scene, Vector3 } from '@babylonjs/core'
 
 export interface CreateHoopParams {
   id: string
@@ -22,11 +22,9 @@ export const createHoop = (params: CreateHoopParams): CreatedHoop => {
 
   const { id, diameter, thickness, tessellation, scene, position, rotation, sensor1Material, sensor2Material } = params
 
-  const parent = new TransformNode('node')
   const hoop = MeshBuilder.CreateTorus(`hoop-${id}`, { diameter, thickness, tessellation }, scene)
-  hoop.parent = parent
-  hoop.rotation = rotation
   hoop.position = position
+  hoop.rotation = rotation
   // hoop.physicsImpostor = new PhysicsImpostor(
   //   hoop,
   //   PhysicsImpostor.BoxImpostor,
